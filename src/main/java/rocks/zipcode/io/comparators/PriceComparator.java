7ad8695d3ec758c68fa26e0aa5a288b47c3b468a
Jacob C <jacobc@Zipcoders-MBP.hsd1.de.comcast.net>
2023-03-26 12:13:25 -0400

    priceComparator passed

--- src/main/java/rocks/zipcode/io/comparators/PriceComparator.java
+++ src/main/java/rocks/zipcode/io/comparators/PriceComparator.java
@@ -1,7 +1,20 @@
 package rocks.zipcode.io.comparators;
 
+import rocks.zipcode.io.Item;
+
+import java.util.Comparator;
+
 /**
  * @author leon on 30/01/2019.
  */
-public class PriceComparator {
-}
+public class PriceComparator implements Comparator<Item> {
+    @Override
+    public int compare(Item i1, Item i2){
+        if (i1.getPrice() > i2.getPrice()){
+            return 1;
+        } else if (i1.getPrice() < i2.getPrice()) {
+            return  -1;
+        }
+            return  0;
+        }
+    }

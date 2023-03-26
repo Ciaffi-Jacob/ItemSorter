232f66769b9ce4e5d385d65c16e887d834ff9717
Jacob C <jacobc@Zipcoders-MBP.hsd1.de.comcast.net>
2023-03-26 11:57:56 -0400

    all item tests passed

--- src/main/java/rocks/zipcode/io/Item.java
+++ src/main/java/rocks/zipcode/io/Item.java
@@ -14,6 +14,9 @@ public class Item {
     }
 
     public Item(Long id, String name, Double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
     }
 
     public String getName() {

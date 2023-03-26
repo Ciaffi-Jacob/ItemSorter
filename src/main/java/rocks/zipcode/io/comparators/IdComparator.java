e16281af5cb5472fcbbfb0132485e7cf6c797749
Jacob C <jacobc@Zipcoders-MBP.hsd1.de.comcast.net>
2023-03-26 12:05:43 -0400

    idComparator passed

--- src/main/java/rocks/zipcode/io/comparators/IdComparator.java
+++ src/main/java/rocks/zipcode/io/comparators/IdComparator.java
@@ -1,7 +1,15 @@
 package rocks.zipcode.io.comparators;
 
+import rocks.zipcode.io.Item;
+
+import java.util.Comparator;
+
 /**
  * @author leon on 30/01/2019.
  */
-public class IdComparator {
+public class IdComparator implements Comparator<Item> {
+    @Override
+    public int compare(Item i1, Item i2){
+        return (int)(i1.getId() - i2.getId());
+    }
 }
